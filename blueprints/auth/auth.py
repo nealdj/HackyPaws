ce46d725d6f8eb174b4cf6ee6a90b2a7d3242ca5
David Neal <davidjn2817@gmail.com>
2024-05-03 19:42:51 -0500

    Authentication is working now

--- blueprints/auth/auth.py
+++ blueprints/auth/auth.py
@@ -1,32 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request
-import datetime, hashlib, hmac, base64
+from hackypaws.Authenticator import Authenticator
 
 auth_bp = Blueprint("auth", __name__)
-
-def generate_session(username):
-    '''
-    Generates user sessions
-        sessions will be invalidated each day for security
-    '''
-    auth_secret = str(datetime.date.today()).encode('utf-8')
-    hmac_value = hmac.new(auth_secret, username.encode('utf-8'), hashlib.md5).digest()
-    session_token = base64.b64encode(hmac_value).decode('utf-8')
-    print(session_token)
-    return session_token
-
-def validate_session(token, username):
-    '''
-    Validates if user token was generated from our site
-    '''
-    token_sig = base64.b64decode(token)
-    auth_secret = str(datetime.date.today()).encode('utf-8')
-    username_bytes = username.encode('utf-8')
-    hmac_digest = hmac.new(auth_secret, username_bytes, hashlib.md5).digest()
-    if hmac.compare_digest(token_sig, hmac_digest):
-        return username
-    else:
-        return False
-    
 
 @auth_bp.route("/")
 def auth_index():
@@ -35,10 +10,13 @@ def auth_index():
 @auth_bp.route("/login", methods=['POST'])
 def login():
     data = request.form
-    username = data.get('username')
-    password = data.get('password')
-    session = generate_session(username)
-    resp = redirect("/")
-    resp.set_cookie('session', session)
-    print(validate_session(session, username))
-    return resp 
+    if not data.get('username') or not data.get('password'):
+        return redirect("/auth?login=False")
+    if not Authenticator.login(data.get('username'), data.get('password')):
+        return redirect("/auth?login=False")
+    else:
+        session = Authenticator.generate_session(data.get('username'))
+        resp = redirect("/admin/")
+        resp.set_cookie('session', session)
+        return resp
+    
